9de644fa4148efac9b838dbf60e1240019e71083
Alexandr <korotkov34reg@gmail.com>
2018-10-18 21:25:57 +0300

    Create XmlParser.java

---
+++ src/main/java/ru/k0r0tk0ff/xml/processor/service/XmlParser.java
@@ -0,0 +1,17 @@
+package ru.k0r0tk0ff.xml.processor.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by korotkov_a_a on 18.10.2018.
+ */
+
+public class XmlParser {
+    private static final Logger LOGGER  = LoggerFactory.getLogger(XmlParser.class);
+    public void testLog() {
+        LOGGER.info("XmlParser");
+    }
+}
+
+
