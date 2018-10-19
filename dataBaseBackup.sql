95a2b2e71bdfd807bb68651e83225c6bd54a3af3
Alexandr <korotkov34reg@gmail.com>
2018-10-19 14:43:24 +0300

    Create dataBaseBackup.sql

---
+++ dataBaseBackup.sql
@@ -0,0 +1,4 @@
+insert into DATA (DEPCODE, DEPJOB, DESCRIPTION) values ('Cru','Checking','Check departament');
+insert into DATA (DEPCODE, DEPJOB, DESCRIPTION) values ('Seq','Sequrity','Guard departament');
+insert into DATA (DEPCODE, DEPJOB, DESCRIPTION) values ('Prod','Prodaction','Maining departament');
+insert into DATA (DEPCODE, DEPJOB, DESCRIPTION) values ('Fin','Finance','Finanse departament');
