78cc57eec400c12cf23b0ae0a71dc20a5fcc7810
Alexandr <korotkov34reg@gmail.com>
2018-10-20 20:46:06 +0300

    Update Main.java

--- src/main/java/ru/k0r0tk0ff/xml/processor/Main.java
+++ src/main/java/ru/k0r0tk0ff/xml/processor/Main.java
@@ -1,8 +1,11 @@
 package ru.k0r0tk0ff.xml.processor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.k0r0tk0ff.xml.processor.dao.DbDao;
 import ru.k0r0tk0ff.xml.processor.infrastructure.io.utils.InputParametersChecker;
 import ru.k0r0tk0ff.xml.processor.infrastructure.store.db.ConnectionHolder;
+import ru.k0r0tk0ff.xml.processor.service.parser.XmlParserException;
 
 /**
  * Created by korotkov_a_a on 18.10.2018.
@@ -14,11 +17,15 @@ public class Main {
         InputParametersChecker.ParametersCheck(args[0], args[1]);
         Process process = new Process();
         process.initializeLoggerSystem();
+        Logger LOGGER = LoggerFactory.getLogger(Main.class);
         process.setDbDao(new DbDao());
         process.createDbTableIfNotExist();
-        process.runMainRoute(args[0], args[1]);
-
-
+        try {
+            process.runMainRoute(args[0], args[1]);
+        } catch (XmlParserException e) {
+            LOGGER.error("Parse XML file error!");
+        }
+        LOGGER.warn("Work complete.");
 
         ConnectionHolder.getInstance().closeConnection();
     }
