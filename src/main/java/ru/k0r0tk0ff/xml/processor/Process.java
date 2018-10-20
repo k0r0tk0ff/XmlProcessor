ea1d888714f646264874c925e05e95909420dfb2
Alexandr <korotkov34reg@gmail.com>
2018-10-20 20:45:38 +0300

    Update Process.java

--- src/main/java/ru/k0r0tk0ff/xml/processor/Process.java
+++ src/main/java/ru/k0r0tk0ff/xml/processor/Process.java
@@ -6,9 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.k0r0tk0ff.xml.processor.dao.DbDao;
 import ru.k0r0tk0ff.xml.processor.domain.RawEntry;
-import ru.k0r0tk0ff.xml.processor.infrastructure.io.utils.AppPropertiesHolder;
-import ru.k0r0tk0ff.xml.processor.service.DataCreatorForXmlFile;
+import ru.k0r0tk0ff.xml.processor.infrastructure.AppPropertiesHolder;
+import ru.k0r0tk0ff.xml.processor.service.DbDataGetter;
+import ru.k0r0tk0ff.xml.processor.service.Synchronizer;
 import ru.k0r0tk0ff.xml.processor.service.XmlFileCreator;
+import ru.k0r0tk0ff.xml.processor.service.parser.XmlParserException;
 
 import javax.xml.stream.XMLStreamException;
 import javax.xml.transform.TransformerException;
@@ -21,6 +23,7 @@ import java.util.Set;
  */
 public class Process {
     private final static String LOG_FILE_NAME_KEY = "xml.processor.log.filename";
+    private final static String LOG_LEVEL_KEY = "xml.processor.log.level";
     private static Logger LOGGER;
 
     private DbDao dbDao;
@@ -31,9 +34,12 @@ public class Process {
 
     public void initializeLoggerSystem() {
         String logFileName;
+        String logLevel;
         loadPropertiesFromFile();
         logFileName = AppPropertiesHolder.getInstance().getLogFilename(LOG_FILE_NAME_KEY);
+        logLevel = AppPropertiesHolder.getInstance().getLogLevel(LOG_LEVEL_KEY);
         System.setProperty(LOG_FILE_NAME_KEY,logFileName);
+        System.setProperty(LOG_LEVEL_KEY,logLevel);
         LoggerContext ctx =
                 (LoggerContext) LogManager.getContext(false);
         ctx.reconfigure();
@@ -52,17 +58,29 @@ public class Process {
         dbDao.createDb();
     }
 
-    public void runMainRoute(String parameter, String fileName) {
+    public void runMainRoute(String parameter, String fileName) throws XmlParserException {
         switch (parameter) {
             case "-G": getDataFromDbAndWriteToFile(fileName); break;
+            case "-C": clearDataInDb(); break;
+            case "-S": synchroDataFromFileWithDataFromDb(fileName); break;
         }
     }
 
+    private void synchroDataFromFileWithDataFromDb(String fileName) throws XmlParserException {
+        Synchronizer synchronizer = new Synchronizer(dbDao, fileName);
+        synchronizer.synchronize();
+    }
+
+    private void clearDataInDb() {
+        dbDao.clearData();
+    }
+
     private void getDataFromDbAndWriteToFile(String filename) {
-        DataCreatorForXmlFile dataCreatorForXmlFile = new DataCreatorForXmlFile(dbDao);
-        Set<RawEntry> entries = dataCreatorForXmlFile.getDataFromDb();
+        DbDataGetter dbDataGetter = new DbDataGetter(dbDao);
+        Set<RawEntry> entries = dbDataGetter.getDataFromDb();
 
         if(LOGGER.isDebugEnabled())
+            LOGGER.debug("Get data from DB:");
         for (RawEntry entry: entries) {
             LOGGER.debug(
                     "RawEntry: " + entry.getDepCode() + " "
@@ -74,11 +92,11 @@ public class Process {
         try {
             xmlFileCreator.convertDataToXmlAndSave();
         } catch (XMLStreamException e) {
-            e.printStackTrace();
+            LOGGER.error("Create XML data error!", e);
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            LOGGER.error("File not found!", e);
         } catch (TransformerException e) {
-            e.printStackTrace();
+            LOGGER.error("XML Transformation error!", e);
         }
 
         LOGGER.info("Get data success. Create file with data -\"" + filename + "\" success.");
