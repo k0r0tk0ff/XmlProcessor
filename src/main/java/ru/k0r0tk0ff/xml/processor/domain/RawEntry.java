7c3ec67da9363fc9e3ed3561828734ee58963c7d
Alexandr <korotkov34reg@gmail.com>
2018-10-20 20:54:07 +0300

    Update RawEntry.java

--- src/main/java/ru/k0r0tk0ff/xml/processor/domain/RawEntry.java
+++ src/main/java/ru/k0r0tk0ff/xml/processor/domain/RawEntry.java
@@ -31,4 +31,23 @@ public class RawEntry {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        RawEntry rawEntry = (RawEntry) o;
+
+        if (!depCode.equals(rawEntry.depCode)) return false;
+        return depJob.equals(rawEntry.depJob);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = depCode.hashCode();
+        result = 31 * result + depJob.hashCode();
+        return result;
+    }
 }
+
