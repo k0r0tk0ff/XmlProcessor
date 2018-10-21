099ae04c7af2fb7b5522d30e7d9bc68afce57bab
Alexandr <korotkov34reg@gmail.com>
2018-10-21 16:25:15 +0300

    Update XmlParser.java

--- src/main/java/ru/k0r0tk0ff/xml/processor/service/parser/XmlParser.java
+++ src/main/java/ru/k0r0tk0ff/xml/processor/service/parser/XmlParser.java
@@ -14,9 +14,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
@@ -26,60 +24,57 @@ import java.util.Set;
 public class XmlParser {
     private static final Logger LOGGER = LoggerFactory.getLogger(XmlParser.class);
 
-    public static Set<RawEntry> getDataFromXmlFile(String fileName) throws XmlParserException  {
-
+    public Set<RawEntry> getDataFromXmlFile(String fileName) throws XmlParserException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         Document document = null;
         try {
             DocumentBuilder builder = factory.newDocumentBuilder();
             document = builder.parse(new File(fileName));
         } catch (ParserConfigurationException e) {
-            LOGGER.error("Parser error!", e);
+            throw new XmlParserException("Parser error!",e);
         } catch (SAXException e) {
-            LOGGER.error("SAX error!", e);
+            throw new XmlParserException("SAX error!",e);
         } catch (IOException e) {
-            LOGGER.error("Read xml file error!", e);
+            throw new XmlParserException("Read xml file error!",e);
         }
-        document.getDocumentElement().normalize();
+
+        if (document.getDocumentElement() != null) {
+            document.getDocumentElement().normalize();
+        }
+
         NodeList nodeList = document.getElementsByTagName("entry");
         if (LOGGER.isDebugEnabled()) {
             LOGGER.debug("See data from loaded xml: ");
         }
-        return convertXmlDataToList(nodeList);
+        return convertXmlDataToSet(nodeList);
     }
 
-    private static Set<RawEntry> convertXmlDataToList(NodeList nodeList) throws XmlParserException {
+    private Set<RawEntry> convertXmlDataToSet(NodeList nodeList) throws XmlParserException {
         Set<RawEntry> dataFromXmlFile = new HashSet<>();
         for (int i = 0; i < nodeList.getLength(); i++) {
             Node node = nodeList.item(i);
             if (node.getNodeType() == Node.ELEMENT_NODE) {
-                Element eElement = (Element) node;
-                RawEntry entry = new RawEntry();
-                entry.setDepCode(eElement.getAttribute("depcode"));
-                entry.setDepJob(eElement.getAttribute("depjob"));
-                entry.setDescription(eElement.getAttribute("description"));
-
-                validateEntry(dataFromXmlFile, entry);
+                RawEntry entry = convertNodeToEntry(node);
+                EntryChecker.checkExistEntryInSet(dataFromXmlFile, entry);
                 dataFromXmlFile.add(entry);
-
-                if (LOGGER.isDebugEnabled()) {
-                    LOGGER.debug("");
-                    LOGGER.debug("depcode : " + eElement.getAttribute("depcode"));
-                    LOGGER.debug("depjob : " + eElement.getAttribute("depjob"));
-                    LOGGER.debug("description : " + eElement.getAttribute("description"));
-                }
             }
         }
         return dataFromXmlFile;
     }
 
-    private static void validateEntry(Set<RawEntry> dataFromXmlFile, RawEntry entry) throws XmlParserException {
-        if (dataFromXmlFile.contains(entry)) {
-            LOGGER.error("Find dublicate in XML file. Please, fix this problem.");
-            LOGGER.error("duplicate depcode : " + entry.getDepCode());
-            LOGGER.error("duplicate depjob : " + entry.getDepJob());
-            throw new XmlParserException();
+    private RawEntry convertNodeToEntry(Node node){
+        Element eElement = (Element) node;
+        RawEntry entry = new RawEntry();
+        entry.setDepCode(eElement.getAttribute("depcode"));
+        entry.setDepJob(eElement.getAttribute("depjob"));
+        entry.setDescription(eElement.getAttribute("description"));
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("");
+            LOGGER.debug("depcode : " + entry.getDepCode());
+            LOGGER.debug("depjob : " + entry.getDepJob());
+            LOGGER.debug("description : " + entry.getDescription());
         }
+        return entry;
     }
 }
 
