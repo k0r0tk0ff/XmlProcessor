cc5e9805ab9170c49e8af3135790cd1a2ff11491
Alexandr <korotkov34reg@gmail.com>
2018-10-21 16:24:48 +0300

    Update and rename DbDataGetter.java to DataConverter.java

--- src/main/java/ru/k0r0tk0ff/xml/processor/service/DataConverter.java
+++ src/main/java/ru/k0r0tk0ff/xml/processor/service/DataConverter.java
@@ -2,7 +2,8 @@ package ru.k0r0tk0ff.xml.processor.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.k0r0tk0ff.xml.processor.dao.DbDao;
+import ru.k0r0tk0ff.xml.processor.dao.DaoException;
+import ru.k0r0tk0ff.xml.processor.dao.H2DbDao;
 import ru.k0r0tk0ff.xml.processor.domain.RawEntry;
 
 import javax.sql.rowset.CachedRowSet;
@@ -13,17 +14,17 @@ import java.util.Set;
 /**
  * Created by korotkov_a_a on 18.10.2018.
  */
-public class DbDataGetter {
-    private final static Logger LOGGER = LoggerFactory.getLogger(DbDataGetter.class);
+public class DataConverter {
+    private final static Logger LOGGER = LoggerFactory.getLogger(DataConverter.class);
 
-    private DbDao dbDao;
+    private H2DbDao h2DbDao;
 
-    public DbDataGetter(DbDao dbDao) {
-        this.dbDao = dbDao;
+    public DataConverter(H2DbDao h2DbDao) {
+        this.h2DbDao = h2DbDao;
     }
 
-    public Set<RawEntry> getDataFromDb() {
-        CachedRowSet cachedRowSet = dbDao.getRawDataFromDb();
+    public Set<RawEntry> convertDbDataToRawEntries() throws DaoException {
+        CachedRowSet cachedRowSet = h2DbDao.getRawDataFromDb();
         Set<RawEntry> entries = new HashSet<>();
 
         try {
