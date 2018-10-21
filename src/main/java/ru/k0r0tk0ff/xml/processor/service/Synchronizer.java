8f06eba1ee8b6918d9e6c98f0a4b520d898bfca7
Alexandr <korotkov34reg@gmail.com>
2018-10-21 16:24:11 +0300

    Update Synchronizer.java

--- src/main/java/ru/k0r0tk0ff/xml/processor/service/Synchronizer.java
+++ src/main/java/ru/k0r0tk0ff/xml/processor/service/Synchronizer.java
@@ -2,12 +2,12 @@ package ru.k0r0tk0ff.xml.processor.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.k0r0tk0ff.xml.processor.dao.DbDao;
+import ru.k0r0tk0ff.xml.processor.dao.DaoException;
+import ru.k0r0tk0ff.xml.processor.dao.H2DbDao;
 import ru.k0r0tk0ff.xml.processor.domain.RawEntry;
 import ru.k0r0tk0ff.xml.processor.service.parser.XmlParser;
 import ru.k0r0tk0ff.xml.processor.service.parser.XmlParserException;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -21,43 +21,44 @@ public class Synchronizer {
     private static final Logger LOGGER = LoggerFactory.getLogger(Synchronizer.class);
 
     private final String fileName;
-    private final DbDao dbDao;
+    private final H2DbDao h2DbDao;
 
-    public Synchronizer(DbDao dbDao, String fileName) {
+    public Synchronizer(H2DbDao h2DbDao, String fileName) {
         this.fileName = fileName;
-        this.dbDao = dbDao;
+        this.h2DbDao = h2DbDao;
     }
 
     private Set<RawEntry> entriesFromFile;
     private Set<RawEntry> entriesFromDb;
 
 
-    public void synchronize() throws XmlParserException {
+    public void synchronize() throws XmlParserException, DaoException {
         initializeDataSets();
         synchronizeData(entriesFromFile, entriesFromDb);
         LOGGER.info("Synchronize with file success.");
     }
 
-    private void initializeDataSets() throws XmlParserException {
-        entriesFromFile = XmlParser.getDataFromXmlFile(fileName);
-        DbDataGetter dbDataGetter = new DbDataGetter(dbDao);
-        entriesFromDb = dbDataGetter.getDataFromDb();
+    private void initializeDataSets() throws XmlParserException, DaoException {
+        XmlParser xmlParser = new XmlParser();
+        entriesFromFile = xmlParser.getDataFromXmlFile(fileName);
+        DataConverter dataConverter = new DataConverter(h2DbDao);
+        entriesFromDb = dataConverter.convertDbDataToRawEntries();
     }
 
-    private void synchronizeData(Set<RawEntry> entriesFromFile, Set<RawEntry> entriesFromDb) {
+    private void synchronizeData(Set<RawEntry> entriesFromFile, Set<RawEntry> entriesFromDb) throws DaoException {
         Set<RawEntry> entriesForDeleteInDb = getEntriesForDeleteInDb(entriesFromFile, entriesFromDb);
         if(!entriesForDeleteInDb.isEmpty()) {
-            dbDao.deleteMissingEntriesInDb(entriesForDeleteInDb);
+            h2DbDao.deleteMissingEntriesInDb(entriesForDeleteInDb);
         }
 
         Set<RawEntry> entriesForInsertToDb = getEntriesForInsertToDb(entriesFromFile, entriesFromDb);
         if(!entriesForInsertToDb.isEmpty()) {
-            dbDao.insertEntriesToDb(entriesForInsertToDb);
+            h2DbDao.insertEntriesToDb(entriesForInsertToDb);
         }
 
         Set<RawEntry> entriesForUpdateInDb = getEntriesForUpdateInDb(entriesFromFile, entriesFromDb);
         if(!entriesForUpdateInDb.isEmpty()) {
-            dbDao.updateEntriesInDb(entriesForUpdateInDb);
+            h2DbDao.updateEntriesInDb(entriesForUpdateInDb);
         }
     }
 
@@ -105,30 +106,30 @@ public class Synchronizer {
         return result;
     }
 
-    private Set<RawEntry> findEntriesForUpdateInDb(Set<RawEntry> setX, Set<RawEntry> setY){
+    private Set<RawEntry> findEntriesForUpdateInDb(Set<RawEntry> entriesFromFile, Set<RawEntry> entriesFromDb){
         Set<RawEntry> result = new HashSet<>();
 
-        Map<Integer, RawEntry> mapInX =
-                setX
+        Map<Integer, RawEntry> theSameEntriesFromFile =
+                entriesFromFile
                 .stream()
-                .filter(setY::contains)
+                .filter(entriesFromDb::contains)
                 .collect(Collectors.toMap(
                 RawEntry::hashCode, x -> x
                 ));
 
-        Map<Integer, RawEntry> mapInY =
-                setY
+        Map<Integer, RawEntry> theSameEntriesFromDb =
+                entriesFromDb
                .stream()
-               .filter(setX::contains)
+               .filter(entriesFromFile::contains)
                .collect(Collectors.toMap(
                        RawEntry::hashCode, x -> x
                ));
 
-        for (Map.Entry<Integer, RawEntry> entry: mapInX.entrySet()) {
+        for (Map.Entry<Integer, RawEntry> entry: theSameEntriesFromFile.entrySet()) {
             Integer hashCode = entry.getKey();
             RawEntry entryFromX = entry.getValue();
             String descrInSetX = entryFromX.getDescription();
-            String descrInSetY = mapInY.get(hashCode).getDescription();
+            String descrInSetY = theSameEntriesFromDb.get(hashCode).getDescription();
             if(!descrInSetX.equals(descrInSetY)) {
                 result.add(entryFromX);
             }
