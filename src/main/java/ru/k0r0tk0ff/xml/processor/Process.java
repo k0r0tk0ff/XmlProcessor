cf99c6bc3236867da3e6edbd44f9af815bf3379c
Alexandr <korotkov34reg@gmail.com>
2018-10-21 16:16:47 +0300

    Update Process.java

--- src/main/java/ru/k0r0tk0ff/xml/processor/Process.java
+++ src/main/java/ru/k0r0tk0ff/xml/processor/Process.java
@@ -1,13 +1,14 @@
 package ru.k0r0tk0ff.xml.processor;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.LoggerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.k0r0tk0ff.xml.processor.dao.DbDao;
+import ru.k0r0tk0ff.xml.processor.dao.DaoException;
+import ru.k0r0tk0ff.xml.processor.dao.H2DbDao;
 import ru.k0r0tk0ff.xml.processor.domain.RawEntry;
-import ru.k0r0tk0ff.xml.processor.infrastructure.AppPropertiesHolder;
-import ru.k0r0tk0ff.xml.processor.service.DbDataGetter;
+import ru.k0r0tk0ff.xml.processor.utils.input.InputParametersChecker;
+import ru.k0r0tk0ff.xml.processor.utils.input.ParametersCheckException;
+import ru.k0r0tk0ff.xml.processor.infrastructure.store.db.ConnectionHolder;
+import ru.k0r0tk0ff.xml.processor.service.DataConverter;
 import ru.k0r0tk0ff.xml.processor.service.Synchronizer;
 import ru.k0r0tk0ff.xml.processor.service.XmlFileCreator;
 import ru.k0r0tk0ff.xml.processor.service.parser.XmlParserException;
@@ -15,75 +16,92 @@ import ru.k0r0tk0ff.xml.processor.service.parser.XmlParserException;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.transform.TransformerException;
 import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Set;
 
 /**
  * Created by korotkov_a_a on 19.10.2018.
  */
 public class Process {
-    private final static String LOG_FILE_NAME_KEY = "xml.processor.log.filename";
-    private final static String LOG_LEVEL_KEY = "xml.processor.log.level";
-    private static Logger LOGGER;
+    private static final Logger LOGGER = LoggerFactory.getLogger(Process.class);
 
-    private DbDao dbDao;
+    private H2DbDao h2DbDao;
+    private String[] args;
+    private Connection connection;
 
-    public void setDbDao(DbDao dbDao) {
-        this.dbDao = dbDao;
+    public Process(H2DbDao h2DbDao, String[] args) {
+        this.h2DbDao = h2DbDao;
+        this.args = args;
+        this.connection = ConnectionHolder.getInstance().getConnection();
     }
 
-    public void initializeLoggerSystem() {
-        String logFileName;
-        String logLevel;
-        loadPropertiesFromFile();
-        logFileName = AppPropertiesHolder.getInstance().getLogFilename(LOG_FILE_NAME_KEY);
-        logLevel = AppPropertiesHolder.getInstance().getLogLevel(LOG_LEVEL_KEY);
-        System.setProperty(LOG_FILE_NAME_KEY,logFileName);
-        System.setProperty(LOG_LEVEL_KEY,logLevel);
-        LoggerContext ctx =
-                (LoggerContext) LogManager.getContext(false);
-        ctx.reconfigure();
-        LOGGER = LoggerFactory.getLogger(Process.class);
-    }
-
-    private void loadPropertiesFromFile() {
-        try {
-            AppPropertiesHolder.getInstance().loadPropertiesFromFile();
-        } catch (IOException e) {
-            System.out.println("Cannot load property file \"" + AppPropertiesHolder.PROPERTIES_FILE_NAME + "\"");
-        }
-    }
-
-    public void createDbTableIfNotExist() {
-        dbDao.createDb();
-    }
-
-    public void runMainRoute(String[] args) throws XmlParserException {
-        String parameter = args[0];
+    public void execute() {
         String fileName = "";
-        if(args.length == 2) {
-            fileName = args[1];
-        }
+        try {
+            InputParametersChecker.parametersCheck(args);
+            prepareWorkSpace();
 
-        switch (parameter) {
-            case "-G": getDataFromDbAndWriteToFile(fileName); break;
-            case "-C": clearDataInDb(); break;
-            case "-S": synchroDataFromFileWithDataFromDb(fileName); break;
+            if(args.length == 2) {
+                fileName = args[1];
+            }
+
+            try {
+                switch (args[0]) {
+                    case "-u":
+                        getDataFromDbAndWriteToFile(fileName);
+                        connection.close();
+                        break;
+                    case "-c":
+                        clearDataInDb();
+                        connection.close();
+                        break;
+                    case "-s":
+                        synchronize(fileName);
+                        connection.close();
+                        break;
+                }
+            } catch (SQLException e) {
+                throw new DaoException("Cannot close connection!",e);
+            }
+            System.out.println("Work complete.");
+        }  catch (XmlParserException e) {
+            LOGGER.error("Parse XML file error!", e);
+        } catch (XMLStreamException e) {
+            LOGGER.error("Create XML data error!", e);
+        } catch (FileNotFoundException e) {
+            LOGGER.error("Incorrect file name for upload file.");
+        } catch (TransformerException e) {
+            LOGGER.error("XML Transformation error!", e);
+        } catch (ParametersCheckException e) {
+            LOGGER.error("Input parameters error!", e);
+        } catch (DaoException e) {
+            LOGGER.error("Data access object exception!", e);
         }
     }
 
-    private void synchroDataFromFileWithDataFromDb(String fileName) throws XmlParserException {
-        Synchronizer synchronizer = new Synchronizer(dbDao, fileName);
+    private void prepareWorkSpace() throws DaoException {
+        h2DbDao.createDb();
+    }
+
+    private void synchronize(String fileName)
+            throws XmlParserException, DaoException {
+        Synchronizer synchronizer = new Synchronizer(h2DbDao, fileName);
         synchronizer.synchronize();
     }
 
-    private void clearDataInDb() {
-        dbDao.clearData();
+    private void clearDataInDb() throws DaoException {
+        h2DbDao.clearData();
     }
 
-    private void getDataFromDbAndWriteToFile(String filename) {
-        DbDataGetter dbDataGetter = new DbDataGetter(dbDao);
-        Set<RawEntry> entries = dbDataGetter.getDataFromDb();
+    private void getDataFromDbAndWriteToFile(String filename) throws
+            ParametersCheckException,
+            FileNotFoundException,
+            XMLStreamException,
+            TransformerException,
+            DaoException {
+        DataConverter dataConverter = new DataConverter(h2DbDao);
+        Set<RawEntry> entries = dataConverter.convertDbDataToRawEntries();
 
         if(LOGGER.isDebugEnabled())
             LOGGER.debug("Get data from DB:");
@@ -95,16 +113,7 @@ public class Process {
         }
 
         XmlFileCreator xmlFileCreator = new XmlFileCreator(entries, filename);
-        try {
-            xmlFileCreator.convertDataToXmlAndSave();
-        } catch (XMLStreamException e) {
-            LOGGER.error("Create XML data error!", e);
-        } catch (FileNotFoundException e) {
-            LOGGER.error("File not found!", e);
-        } catch (TransformerException e) {
-            LOGGER.error("XML Transformation error!", e);
-        }
-
+        xmlFileCreator.convertDataToXmlAndSave();
         LOGGER.info("Get data success. Create file with data -\"" + filename + "\" success.");
     }
 }
