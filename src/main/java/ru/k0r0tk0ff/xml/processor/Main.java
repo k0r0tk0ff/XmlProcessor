d7c30b50ce06466308cb55e06423371df5236c67
Alexandr <korotkov34reg@gmail.com>
2018-10-22 10:55:28 +0300

    Update Main.java

--- src/main/java/ru/k0r0tk0ff/xml/processor/Main.java
+++ src/main/java/ru/k0r0tk0ff/xml/processor/Main.java
@@ -3,7 +3,8 @@ package ru.k0r0tk0ff.xml.processor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.LoggerContext;
 import ru.k0r0tk0ff.xml.processor.dao.H2DbDao;
-import ru.k0r0tk0ff.xml.processor.infrastructure.AppPropertiesHolder;
+import ru.k0r0tk0ff.xml.processor.properties.AppPropertiesException;
+import ru.k0r0tk0ff.xml.processor.properties.AppPropertiesHolder;
 
 import java.io.IOException;
 
@@ -13,21 +14,22 @@ import java.io.IOException;
 
 public class Main {
 
-    static {
+    public static void main(String[] args) {
         try {
             initializeLoggerSystem();
+            Process process = new Process(new H2DbDao(), args);
+            process.execute();
         } catch (IOException e) {
             System.out.println("Read the property file error! Check that the file exists and contains valid data.");
+        } catch (AppPropertiesException e) {
+            System.out.println(e.getMessage());
         }
     }
-    public static void main(String[] args) {
-        Process process = new Process(new H2DbDao(), args);
-        process.execute();
-    }
 
-    private static void initializeLoggerSystem() throws IOException {
+    private static void initializeLoggerSystem() throws IOException, AppPropertiesException {
         AppPropertiesHolder appProperties = AppPropertiesHolder.getInstance();
         appProperties.loadPropertiesFromFile();
+        appProperties.checkDataBaseProperties();
         appProperties.setLogFileName();
         appProperties.setLogLevel();
         LoggerContext ctx =
